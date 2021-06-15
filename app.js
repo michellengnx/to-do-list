e90e3b16c1bd90189f0cdde2b4fb8596c7b509e9
michellengnx <michellenguyen1608@gmail.com>
2021-06-15 15:07:38 -0400

    finished to-do list

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { urlencoded } = require("body-parser");
+// const { urlencoded } = require("body-parser");
 const mongoose = require("mongoose");
 var _ = require('lodash');
 
